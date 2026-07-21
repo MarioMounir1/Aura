846e248c366eced943e6858610ac925b10f0ef9e
Mario Mounir <maryomoner58@gmail.com>
2026-07-21 05:44:05 +0300

    Fix Android MainActivity to inherit from FlutterFragmentActivity

--- mobile/android/app/src/main/kotlin/com/example/calc_calories/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/example/calc_calories/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.calc_calories
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
