9e252a6e33e9adc70241d23625b474983666b13a
Mario Mounir <maryomoner58@gmail.com>
2026-07-31 10:51:09 +0300

    fix(android): update Application ID to com.mario.Aura for Google Play release

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.calc_calories"
+    namespace = "com.mario.aura"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.calc_calories"
+        // Production unique Application ID for Google Play Console
+        applicationId = "com.mario.Aura"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
