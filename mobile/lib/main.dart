1c88435f57173863baf341cdc9dacd901c54af11
Mario Mounir <maryomoner58@gmail.com>
2026-08-12 15:17:45 +0300

    update pre-screen display timer to 4 seconds

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -325,8 +325,8 @@ class _AuthWrapperState extends State<AuthWrapper> {
   @override
   void initState() {
     super.initState();
-    // Guarantee pre-screen logo animation is displayed for at least 2 seconds
-    Future.delayed(const Duration(seconds: 2), () {
+    // Guarantee pre-screen logo animation is displayed for at least 4 seconds
+    Future.delayed(const Duration(seconds: 4), () {
       if (mounted) {
         setState(() {
           _isMinPreScreenTimeElapsed = true;
