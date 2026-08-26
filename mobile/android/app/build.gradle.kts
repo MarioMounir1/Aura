4e3ac4826df683f225cfba47c3e5b84863c71241
Mario Mounir <maryomoner58@gmail.com>
2026-08-26 17:44:54 +0300

    fix: remove release signing from debug builds

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -46,9 +46,6 @@ android {
     }
 
     buildTypes {
-        debug {
-            signingConfig = signingConfigs.getByName("release")
-        }
         release {
             signingConfig = signingConfigs.getByName("release")
         }
